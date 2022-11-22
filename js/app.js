b09375d8c433b4a78f15b280bd111913760ee583
Esau <esaurenteria.isc@gmail.com>
2022-11-22 13:44:01 -0600

    Agregando funcion para agregar los años al select mediante los datos de la BD

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 //Variables
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
 
+const max = new Date().getFullYear();
+const min = max -10
+
+console.log(max)
+console.log(min);
 
 
 //Eventos
 document.addEventListener('DOMContentLoaded', () => {
-mostrarAutos();
+mostrarAutos(); // Muestra los automoviles al cargar
+
+llenarSelect(); //LLena las opciones de años
 })
 
 function mostrarAutos(){
@@ -22,10 +30,24 @@ function mostrarAutos(){
 
 
 
-        //
+        //Inserta en el HTML
         resultado.appendChild(autoHTML)
 
     })
 }
 
 //Funciones
+
+
+//Genera los años del select
+function llenarSelect(){
+    
+
+    for(let i = max; i>=min; i-- ){
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent=i;
+        year.appendChild(opcion); //agrega las opciones de año al select
+    }
+
+}
